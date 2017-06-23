514bd1c60f42d690fcfcc4144a4340e0236b800a
Dave <wm.david.gallup@gmail.com>
2017-06-23 15:01:28 -0700

    Update migration files. Closes #8

--- db/migrations/20170623083837_earthquakes.js
+++ db/migrations/20170623083837_earthquakes.js
@@ -3,9 +3,14 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('earthquakes', (table) => {
     table.increments('id').primary();
     table.dateTime('event_date_time');
+    table.integer('tz_offset');
+    table.dateTime('last_updated');
     table.string('event_lat');
     table.string('event_long');
-    table.float('magnitude', 2, 1);
+    table.foat('depth', 8, 2);
+    table.float('magnitude', 3, 2);
+    table.string('description');
+    table.integer('usgs_id');
 };
 
 exports.down = function(knex, Promise) {
--- db/migrations/20170623084959_users.js
+++ db/migrations/20170623084959_users.js
@@ -3,8 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
     table.string('name').notNullable().defaultTo('');
-    table.string('email').unique().notNullable().defaultTo('');
+    table.string('email').unique().notNullable();
     table.specificType('hashed_password', 'char(60)').notNullable();
+    table.integer('timezone');
   });
 };
 
