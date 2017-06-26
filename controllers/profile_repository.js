672dcacf6f7218f409971dffac02bb3c8ac88052
SSabu <sandeepsabu09@gmail.com>
2017-06-26 14:58:41 -0700

    Add profile controllers, user and earthquake routers

---
+++ controllers/profile_repository.js
@@ -0,0 +1,142 @@
+const knex = require('../knex.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+class Profile = {
+
+  // lookup user by email
+
+  getUserName(email){
+  return knex('users').first().where('email', email)
+}
+
+  // compare password to hashed_password
+
+  checkPassword(email, password){
+		return knex('users').select('hashed_password').first().where({email})
+		.then(queryResult => {
+			let hashed = queryResult.hashed_password;
+			return bcrypt.compare(password, hashed);
+		})
+	}
+
+  // insert profile into users database /
+
+  addUser(user) {
+    return knex('users').insert(user,'*');
+  }
+
+  // return all public user data from users table
+
+  queryAllUsers() {
+    return knex('users').select('name','email','timezone');
+  }
+
+  // delete users profile
+
+  deleteUser(id) {
+    return knex('users')
+      .del()
+      .where('id', id)
+      .returning('*');
+  }
+
+  // update users profile
+
+  updateUser(id, user) {
+    return knex('users')
+      .where({ id })
+      .update({
+        name: user.name,
+        email: user.email,
+        timezone: user.timezone}, '*');
+  }
+
+  // lookup all saved earthquakes in a user's profile
+
+  querySavedEarthquakes(id){
+    return knex('saved_earthquakes').where('user_id', id);
+  }
+
+  // post an event to a users profile
+
+  addEvent(id) {
+    return knex('saved_earthquakes').insert(id,'*');
+  }
+
+  // delete an event from a users profile
+
+  deleteUser(id) {
+    return knex('saved_earthquakes')
+      .del()
+      .where('event_id', id)
+      .returning('*');
+  }
+
+  // return all public notes from a users profile
+
+  queryAllNotesByUser() {
+    return knex('notes').where('is_private', false);
+  }
+
+  //  post a note to a users profile
+
+  addNote(note) {
+    return knex('notes').insert(note,'*');
+  }
+
+  // delete a note from a users profile
+
+  deleteNote(id) {
+    return knex('notes')
+      .del()
+      .where('id', id)
+      .returning('*');
+  }
+
+  // return a list of friends for a user
+
+  queryAllFriends(id) {
+    return knex('friends').where('user_from', id);
+  }
+
+  // post a friend to a user's friends table
+
+  addFriend(user_from, user_to) {
+    return knex('friends').where({ user_from }).insert(user_to,'*');
+  }
+
+  // delete a friend from a users friend table
+
+  deleteFriend(user_from, user_to) {
+    return knex('friends')
+      .del()
+      .where( { user_from, user_to })
+      .returning('*');
+  }
+
+  // return a list of poi's for a user
+
+  queryAllPOIs(user_id) {
+    return knex('points_of_interest').where({ user_id });
+  }
+
+  // post a poi for a user_id
+
+  addPOI(user_id, poi) {
+    return knex('points_of_interests').where({ user_id }).insert(poi,'*');
+  }
+
+  // delete a poi for a user
+
+  deletePOI(user_id, id) {
+    return knex('points_of_interests')
+      .del()
+      .where({ user_id, id })
+      .returning('*');
+  }
+
+
+}
+
+module.exports = Profile;
--- routes/earthquake.js
+++ routes/earthquake.js
@@ -5,6 +5,36 @@ const knex       = require('../knex');
 const Eartquake  = require('../controllers/earthquake_repostiory');
 const router     = express.Router();
 
+/**
+* @api {get} /earthquake/:id  Request information about a specific earthquake
+* @apiName GetEarthquakeById
+* @apiGroup Earthquake
+*
+* @apiParam {Number} id                  Earthquake's unique ID
+*
+* @apiSuccess {Integer} id               ID of the note.
+* @apiSuccess {Integer} user_id          ID of the user who wrote the note.
+* @apiSuccess {Integer} event_id         ID of the event the note was about.
+* @apiSuccess {String} note_date_time    Date and time the note was created.
+* @apiSuccess {String} text              Text of the note.
+*
+* @apiSuccessExample Success-Response:
+*   HTTP/1.1 200 OK
+*  {
+*  id: 4,
+*  description: "5km NNE of Bainbridge Island, Washington",
+*  date_time: 2017-06-23T13:42:24.000Z,
+*  last_updated: 2017-06-23T14:42:24.000Z,
+*  tz_offset: -480,
+*  magnitude: 1.1,
+*  long:  '-122.4905',
+*  lat:  '47.6686667',
+*  depth:  4.58,
+*  usgs_id: "uw61276557"
+*  }
+*
+*/
+
 router.get('/:id', (req, res) => {
   let earthquake = new Earthquake();
   let id = req.params.id;
@@ -20,4 +50,50 @@ router.get('/:id', (req, res) => {
     .catch(err => {
       res.status(500).send(err);
     });
-}
+});
+
+/**
+* @api {get} /earthquake/:id/notes  Request public notes posted about a specific earthquake
+* @apiName GetNotesByEarthquakeId
+* @apiGroup Earthquake
+*
+* @apiParam {Number} id                  Earthquake's unique ID
+*
+* @apiSuccess {Integer} id               ID of the note.
+* @apiSuccess {Integer} user_id          ID of the user who wrote the note.
+* @apiSuccess {Integer} event_id         ID of the event the note was about.
+* @apiSuccess {String} note_date_time    Date and time the note was created.
+* @apiSuccess {String} text              Text of the note.
+*
+* @apiSuccessExample Success-Response:
+*   HTTP/1.1 200 OK
+*   {
+*   id: 3,
+*   user_id: 3,
+*   event_id: 3,
+*   note_date_time: 2017-06-23T13:42:24.000Z,
+*   text: 'I survived this monster!'
+*   }
+*
+*/
+
+router.get('/:id/notes', (req, res) => {
+  let earthquake = new Earthquake();
+  let id = req.params.id;
+  let promiseFromQuery = earthquake.notesById(id);
+
+  promiseFromQuery
+    .then(notes => {
+      if(!notes) {
+        res.sendStatus(404);
+        return;
+      }
+      res.send(notes);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+
+module.exports = router;
