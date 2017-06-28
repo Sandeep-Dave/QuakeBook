bb145c486f2df2c25df89a038cddfed4a30c938a
Dave <wm.david.gallup@gmail.com>
2017-06-28 08:21:37 -0700

    Build profile routes - core

--- routes/profile.js
+++ routes/profile.js
@@ -4,6 +4,7 @@ const express       = require('express');
 const router        = express.Router();
 const jwt           = require('jsonwebtoken');
 const env           = require('dotenv');
+const bcrypt        = require('bcrypt');
 const checkForToken = require('./helpers').checkForToken;
 const verifyUser    = require('./helpers').verifyUser;
 const ProfileRepo   = require('../controllers/profile_repository');
@@ -36,16 +37,24 @@ router.post('/login', (req, res) => {
   const repo      = new ProfileRepo();
   const email     = req.body.email;
   const password  = req.body.password;
+  var user_id;
 
-  repo.checkPassword(email, password)
-    .then((verified) => {
-
-      if(verified < 0){
+  repo.getHashedPassword(email)
+    .then((resolved) => {
+      if(resolved === undefined){
         res.setHeader('Content-Type', 'plain/text');
         res.status(404).send(`Not Found`);
         return;
       }
-
+      user_id = resolved.id;
+      return bcrypt.compare(password, resolved.hashed_password);
+    })
+    .then((isMatched) => {
+      if(!isMatched){
+        res.setHeader('Content-Type', 'plain/text');
+        res.status(403).send(`Not Authorized`);
+        return;
+      }
       let payload = {
   	    iss: 'jwt_lesson_app',
   	    sub: {
@@ -128,10 +137,7 @@ router.get('/', checkForToken, verifyUser, (req, res) => {
 * @apiSuccessExample Success-Response:
 *   HTTP/1.1 200 OK
 *   {
-*     id: 55,
-      name: John Doe,
-      email: jdoe@domain.com,
-      timezone = -3
+*     id: 55
 *   }
 *
 * @apiError NotValid The supplied user info was not valid.
@@ -143,85 +149,192 @@ router.get('/', checkForToken, verifyUser, (req, res) => {
 */
 router.put('/', (req, res) => {
 
-  
+  const repo = new ProfileRepo();
+
+  if(!req.body || !req.body.name || !req.body.email || !req.body.password || !req.body.timezone){
+    res.setHeader('Content-Type', 'plain/text');
+    res.status(400).send(`Invalid User Information`);
+    return;
+  }
+
+  var user = {name: req.body.name
+            , email: req.body.email
+            , timezone: req.body.timezone};
+
+  bcrypt.hash(req.body.password, 12)
+  .then((hash) => {
+    user.hashed_password = hash;
+    return repo.addUser(user);
+  })
+  .then((addedUserId) => {
+    if(addedUserId < 0){
+      res.setHeader('Content-Type', 'plain/text');
+      res.status(400).send(`Invalid User Information`);
+      return;
+    }
+    res.send(addedUserId);
+  });
 });
 
 /**
-* @api {post} /profile
+* @api {post} /profile    Update user profile information
 * @apiName UpdateProfile
 * @apiGroup Profile
 *
 * @apiParam {Number} id                  User's unique ID
 *
-* @apiSuccess {Integer} id               ID of the note.
+* @apiSuccess {Integer} id               ID of the user.
+* @apiSuccess {String} name              User's name
+* @apiSuccess {String} email             User's email address
+* @apiSuccess {Integer} timezone         User's timezone
 *
 * @apiSuccessExample Success-Response:
 *   HTTP/1.1 200 OK
 *   {
-*
+*     id: 67,
+      name: Phil Hartman,
+      email: cavemanlawyer@mindspring.org,
+      timezone: 9
 *   }
 *
-* @apiError NoteNotFound The note was not found.
+* @apiError UserNotFound The user was not found.
 * @apiErrorExample {json} Not Found Error:
 *     HTTP/1.1 404 Not Found
 *     {
-*       "error": "NoteNotFound"
+*       "error": "UserNotFound"
 *     }
 */
-router.post('/', (req, res) => {
+router.post('/', checkForToken, verifyUser, (req, res) => {
 
+  if(!req.body || (!req.body.name && !req.body.email && !req.body.timzone && !req.body.password)){
+    res.setHeader('Content-Type', 'plain/text');
+    res.status(400).send(`Invalid User Information`);
+    return;
+  }
+
+  let changes = {};
+  if(req.body.name){
+    changes.name = req.body.name;
+  }
+  if(req.body.email){
+    changes.email = req.body.email;
+  }
+  if(req.body.timezone){
+    changes.timezone = req.body.timezone;
+  }
+
+  var returned_hash = Promise.resolve(false);
+  if(req.body.password){
+    returned_hash = bcrypt.hash(req.body.password, 12);
+  }
+  const repo    = new ProfileRepo();
+  const decoded = jwt.decode(req.cookies.token);
+
+  returned_hash
+    .then((resolved_password) => {
+      if(resolved_password){
+        changes.hashed_password = resolved_password;
+      }
+      return repo.updateUser(decoded.sub.user_id, changes);
+    })
+    .then((updatedUser) => {
+      if(updatedUser === undefined){
+        res.setHeader('Content-Type', 'plain/text');
+        res.status(400).send(`Invalid User Information`);
+        return;
+      }
+      res.send(updatedUser);
+    });
 });
 
 /**
-* @api {delete} /profile
+* @api {delete} /profile    Deletes user profile
 * @apiName DeleteProfile
 * @apiGroup Profile
 *
 * @apiParam {Number} id                  User's unique ID
 *
-* @apiSuccess {Integer} id               ID of the note.
+* @apiSuccess {Integer} id               ID of the User.
+* @apiSuccess {String} name              User's name
+* @apiSuccess {String} email             User's email
+* @apiSuccess {Integer} timezone         User's timezone
 *
 * @apiSuccessExample Success-Response:
 *   HTTP/1.1 200 OK
 *   {
-*
+*     id: 18,
+*     name: Helen Miren,
+*     email: iwasinexcalliber@yahoo.com,
+*     timezone: 0
 *   }
 *
-* @apiError NoteNotFound The note was not found.
+* @apiError UserNotFound The user was not found.
 * @apiErrorExample {json} Not Found Error:
 *     HTTP/1.1 404 Not Found
 *     {
-*       "error": "NoteNotFound"
+*       "error": "User Not found"
 *     }
 */
-router.delete('/', (req, res) => {
+router.delete('/', checkForToken, verifyUser, (req, res) => {
+  const repo    = new ProfileRepo();
+  const decoded = jwt.decode(req.cookies.token);
+
+  repo.deleteUser(decoded.sub.user_id)
+    .then((deletedUser) => {
+      if(deletedUser === undefined){
+        res.setHeader('Content-Type', 'plain/text');
+        res.status(400).send(`Invalid User Information`);
+        return;
+      }
+      res.send(deletedUser);
+    });
 
 });
 
 /**
-* @api {get} /profile/earthquakes
+* @api {get} /profile/earthquakes   Return all earthquakes saved in profile
 * @apiName GetProfileEarthquakes
 * @apiGroup Profile
 *
 * @apiParam {Number} id                  User's unique ID
 *
-* @apiSuccess {Integer} id               ID of the note.
+* @apiSuccess {Number} id               ID of the note.
+* @apiSuccess {String} date_time               ID of the note.
+* @apiSuccess {Number} tz_offset               ID of the note.
+* @apiSuccess {String} last_updated               ID of the note.
+* @apiSuccess {String} lat               ID of the note.
+* @apiSuccess {String} long               ID of the note.
+* @apiSuccess {Number} depth               ID of the note.
+* @apiSuccess {Number} magnitude               ID of the note.
+* @apiSuccess {String} description               ID of the note.
+* @apiSuccess {String} usgs_id               ID of the note.
 *
 * @apiSuccessExample Success-Response:
 *   HTTP/1.1 200 OK
-*   {
+*   [
+*    {
+*     id: ,
+*     date_time: ,
+*     tz_offset: ,
+*     last_updated: ,
+*     lat: ,
+*     long: ,
+*     depth: ,
+*     magnitude: ,
+*     description: ,
+*     usgs_id:
+*    }
+*   ]
 *
-*   }
-*
-* @apiError NoteNotFound The note was not found.
+* @apiError EventNotFound The earthquake event was not found.
 * @apiErrorExample {json} Not Found Error:
 *     HTTP/1.1 404 Not Found
 *     {
-*       "error": "NoteNotFound"
+*       "error": "EventNotFound"
 *     }
 */
-router.get('/earthquakes', (req, res) => {
-
+router.get('/earthquakes', checkForToken, verifyUser, (req, res) => {
+  
 });
 
 /**
@@ -246,7 +359,7 @@ router.get('/earthquakes', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.put('/earthquakes', (req, res) => {
+router.put('/earthquakes', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -272,7 +385,7 @@ router.put('/earthquakes', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.delete('/earthquake/:id', (req, res) => {
+router.delete('/earthquake/:id', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -298,7 +411,7 @@ router.delete('/earthquake/:id', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.get('/notes', (req, res) => {
+router.get('/notes', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -324,7 +437,7 @@ router.get('/notes', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.put('/notes', (req, res) => {
+router.put('/notes', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -350,7 +463,7 @@ router.put('/notes', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.post('/notes/:id', (req, res) => {
+router.post('/notes/:id', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -376,7 +489,7 @@ router.post('/notes/:id', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.delete('/notes/:id', (req, res) => {
+router.delete('/notes/:id', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -402,7 +515,7 @@ router.delete('/notes/:id', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.get('/friends', (req, res) => {
+router.get('/friends', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -428,7 +541,7 @@ router.get('/friends', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.put('/friends/:id', (req, res) => {
+router.put('/friends/:id', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -454,7 +567,7 @@ router.put('/friends/:id', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.delete('/friends/:id', (req, res) => {
+router.delete('/friends/:id', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -480,7 +593,7 @@ router.delete('/friends/:id', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.get('/poi', (req, res) => {
+router.get('/poi', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -506,7 +619,7 @@ router.get('/poi', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.put('/poi', (req, res) => {
+router.put('/poi', checkForToken, verifyUser, (req, res) => {
 
 });
 
@@ -532,7 +645,7 @@ router.put('/poi', (req, res) => {
 *       "error": "NoteNotFound"
 *     }
 */
-router.delete('/poi/:id', (req, res) => {
+router.delete('/poi/:id', checkForToken, verifyUser, (req, res) => {
 
 });
 
